d5913856ce999f3aee0cff164f56016edaa78e48
MagicianJZ <justinzhang201009@gmail.com>
2026-07-06 10:10:53 -0700

    LSDV3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
@@ -75,10 +75,17 @@ function App() {
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClientInstance}>
-        <Router>
-          <ScrollToTop />
-          <AuthenticatedApp />
-        </Router>
+        {import.meta.env.PROD ? (
+          <HashRouter>
+            <ScrollToTop />
+            <AuthenticatedApp />
+          </HashRouter>
+        ) : (
+          <BrowserRouter>
+            <ScrollToTop />
+            <AuthenticatedApp />
+          </BrowserRouter>
+        )}
         <Toaster />
       </QueryClientProvider>
     </AuthProvider>
